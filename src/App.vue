cd2fc565dc1e86dd7722bdc7020b53ab0c7832cf
braden <braden@roundupapp.com>
2023-02-24 08:20:13 -0700

    downgrade vuetify and vue

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,14 @@
     <v-app-bar>
       <v-container class="d-flex align-center py-0">
         <v-app-bar-title class="pl-0">
-          <div class="d-flex align-center">
-
-            Ribbon
-          </div>
+          <div class="d-flex align-center">Ribbon</div>
         </v-app-bar-title>
       </v-container>
     </v-app-bar>
 
     <v-main>
       <section id="hero">
-        <v-sheet
-          class="d-flex align-center pb-16"
-          color="grey-darken-3"
-        >
+        <v-sheet class="d-flex align-center pb-16" color="grey-darken-3">
           <v-container class="text-center">
             <v-responsive class="mx-auto" width="500">
               <v-img
@@ -27,7 +21,9 @@
               <h3 class="text-h3">Try Ribbon's all new features</h3>
 
               <p class="mt-4 text-medium-emphasis">
-                Our all-in-one platform gives you the banking, accounting, fundraising, and organizational tools you need to build a successful charity under the umbrella of your fiscal sponsor.
+                Our all-in-one platform gives you the banking, accounting,
+                fundraising, and organizational tools you need to build a
+                successful charity under the umbrella of your fiscal sponsor.
               </p>
             </v-responsive>
           </v-container>
@@ -39,88 +35,63 @@
           <v-container>
             <v-row justify="space-between">
               <v-col cols="auto">
-                <h2 class="text-h4">
-                  Ribbon Donor List
-                </h2>
+                <h2 class="text-h4">Ribbon Donor List</h2>
 
-                <p class="text-primary mt-3">
-                  In Beta now!
-                </p>
+                <p class="text-primary mt-3">In Beta now!</p>
 
-                <p class="mt-3">
-                  See all those that have given in one place!
-                </p>
+                <p class="mt-3">See all those that have given in one place!</p>
               </v-col>
-
             </v-row>
             <v-row justify="space-between">
               <v-col cols="auto">
-                  <v-table v-if="donors" >
-                    <thead>
+                <v-table v-if="donors">
+                  <thead>
                     <tr>
-                      <th class="text-left">
-                        Name
-                      </th>
-                      <th class="text-left">
-                        Email
-                      </th>
-                      <th class="text-left">
-                        Total Donations
-                      </th>
-                      <th class="text-left">
-                        First Donation
-                      </th>
+                      <th class="text-left">Name</th>
+                      <th class="text-left">Email</th>
+                      <th class="text-left">Total Donations</th>
+                      <th class="text-left">First Donation</th>
                     </tr>
-                    </thead>
-                    <tbody>
-                    <tr
-                      v-for="item in donors.data"
-                      :key="item.id"
-                    >
+                  </thead>
+                  <tbody>
+                    <tr v-for="item in donors.data" :key="item.id">
                       <td>{{ item.full_name }}</td>
                       <td>{{ item.email }}</td>
                       <td>{{ item.total_donations }}</td>
                       <td>{{ item.first_donation }}</td>
                     </tr>
-                    </tbody>
-                  </v-table>
-
-
-
+                  </tbody>
+                </v-table>
               </v-col>
-
             </v-row>
           </v-container>
         </section>
       </v-sheet>
 
-      <v-sheet
-        class="py-16"
-        color="#181818"
-      >
+      <v-sheet class="py-16" color="#181818">
         <section id="grid">
           <v-container>
             <v-row justify="space-between">
               <v-col cols="auto">
                 <v-responsive width="350">
-                     <h2 class="text-h4">
-                    Show your support feature
-                  </h2>
-            <p class="text-success mt-3">
-              Available now!
-            </p>
+                  <h2 class="text-h4">Show your support feature</h2>
+                  <p class="text-success mt-3">Available now!</p>
                   <p class="mt-3">
                     Easily send messages to those that have given!
                   </p>
                 </v-responsive>
               </v-col>
               <v-sheet width="400" class="mx-auto">
-                <v-form v-model="valid" validate-on="submit" @submit.prevent="submit">
+                <v-form
+                  v-model="valid"
+                  validate-on="submit"
+                  @submit.prevent="submit"
+                >
                   <v-textarea
                     v-model="message"
                     :rules="messageRules"
                     label="Message"
-                  ></v-textarea >
+                  ></v-textarea>
                   <v-text-field
                     v-model="email"
                     :rules="emailRules"
@@ -140,10 +111,10 @@
     </v-main>
 
     <v-footer>
-      <v-container class="text-overline d-flex align-center justify-space-between">
-        <div>
-          Copyright &copy; 2023 Ribbon, LLC
-        </div>
+      <v-container
+        class="text-overline d-flex align-center justify-space-between"
+      >
+        <div>Copyright &copy; 2023 Ribbon, LLC</div>
 
         <v-icon icon="mdi-bank" size="x-large" />
       </v-container>
@@ -152,42 +123,42 @@
 </template>
 
 <script>
-
 import axios from "axios";
-  export default { name: 'App',
+export default {
+  name: "App",
 
-    data () {
-      return {
-          donors: null,
-          valid: false,
-          email: '',
-        donor_id: '',
-        message: '',
-        emailRules: [
-          value => {
-            if (value) return true
+  data() {
+    return {
+      donors: null,
+      valid: false,
+      email: "",
+      donor_id: "",
+      message: "",
+      emailRules: [
+        (value) => {
+          if (value) return true;
 
-            return 'E-mail is required.'
-          },
-        ],
-        messageRules: [
-          value => {
-            if (value) return true
+          return "E-mail is required.";
+        },
+      ],
+      messageRules: [
+        (value) => {
+          if (value) return true;
 
-            return 'Message is required.'
-          },
-        ],
-      }
+          return "Message is required.";
+        },
+      ],
+    };
+  },
+  mounted() {
+    axios
+      .get("https://interview.ribbon.giving/api/donors")
+      .then((response) => (this.donors = response.data));
+  },
+  methods: {
+    async submit() {
+      // TODO send message to donor
     },
-    mounted () {
-      axios
-        .get('https://interview.ribbon.giving/api/donors')
-        .then(response => (this.donors = response.data))
-    },
-    methods: {
-      async submit () {
-       // TODO send message to donor
-      },
-    }
-  }
+  },
+};
 </script>
